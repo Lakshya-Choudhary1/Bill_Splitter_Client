2e05da4aa6b28c661e3e60e25115ec1e510dbaa2
LAKSHYA CHOUDHARY <choudharylakshya.lc.com>
2026-06-30 00:38:14 +0530

    added tailwindcss react-hot-toast lucide-react

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
+import {Routes,Route} from "react-router-dom";
+
+import toast from 'react-hot-toast';
 
 const App = () => {
-  return (
-    <div>
-      <h1>BILL SPLITTER CLIENT</h1>
-    </div>
+
+  toast.success("Expense added");
+
+
+  return (<>
+    
+    <Routes>
+      <Route path='/' element={<h1>hii</h1>
+      }></Route>
+
+    </Routes>
+  </>
   )
 }
 
